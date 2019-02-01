514095564123e55dcab97ed0dcdcbee26d296847
rajat.sharma <rajats1.kv2srg@gmail.com>
2019-02-01 19:58:36 +0530

    transitionEffect_Design_improv

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
-var colors = generateRandomColor(6);
+var numSquares =6;
 
+var colors = generateRandomColor(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor =pickColor() ;
 var colorDisplay = document.getElementById('colorDisplay');
@@ -7,18 +8,58 @@ colorDisplay.textContent = pickedColor;
 var resetbutton = document.getElementById('reset');
 var messageDisplay = document.getElementById('message');
 var h1 = document.querySelector('h1');
+var easyBtn = document.querySelector('#easyBtn');
+var hardBtn = document.querySelector('#hardBtn');
+
+easyBtn.addEventListener("click",function(){
+	easyBtn.classList.add("selected");
+	hardBtn.classList.remove("selected");
+	numSquares=3;
+	colors=generateRandomColor(numSquares);
+	pickedColor=pickColor();
+	colorDisplay.textContent=pickedColor;
+	for (var i = 0; i < squares.length; i++) {
+		if (colors[i]) {
+			squares[i].style.backgroundColor=colors[i];
+		}
+		else{
+			squares[i].style.display="none"	;
+		} 
+	}
+
+});
+
+
+hardBtn.addEventListener("click",function(){
+	hardBtn.classList.add("selected");
+	easyBtn.classList.remove("selected");
+	numSquares=6;
+	colors=generateRandomColor(numSquares);
+	pickedColor=pickColor();
+	colorDisplay.textContent=pickedColor;
+	for (var i = 0; i < squares.length; i++) {
+			squares[i].style.backgroundColor=colors[i];
+			squares[i].style.display="block"	;
+	}
+
+});
+
+
+
+
 resetbutton.addEventListener("click",function(){
-	colors=generateRandomColor(6);
+	colors=generateRandomColor(numSquares);
 	pickedColor=pickColor();
 	colorDisplay.textContent=pickedColor;
 
 	for (var i = 0; i < squares.length; i++) {
 			squares[i].style.backgroundColor=colors[i];
 	}
-	h1.style.backgroundColor="darkgrey";
+	h1.style.backgroundColor="steelblue";
 	document.getElementById('message').textContent="";
 
 });
+
 for (var i = 0; i < squares.length; i++) {
 	//initial colors
 	squares[i].style.backgroundColor=colors[i];
